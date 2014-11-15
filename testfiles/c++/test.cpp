8bf93caa3eace642e66cf330a84719a0fed09c24
makinj <basicislife@gmail.com>
2014-11-15 15:16:12 +0000

    adds first test file

---
+++ testfiles/c++/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+bool find_matches(int array[], int n){
+  //O(n^2)
+  for (int x=0;x<n-1;x++){
+    for (int y=x+1;y<n;y++){
+      if (array[x]==-array[y]){
+        std::cout<<array[x]<<array[y]<<std::endl;
+        return 1;
+      }
+    }
+  }
+  return 0;
+}
+
+int main(){
+  int v[]={5,3,65,7,4,-2,-2,4,5,-7,3,-2};
+  std::cout<<find_matches(v,12)<<std::endl;
+  return 0;
+}
