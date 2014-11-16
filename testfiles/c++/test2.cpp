0c2d88554867b0986f1a32b118fc8cece94fe3eb
Eric Oswald <eoswald39@gmail.com>
2014-11-16 07:13:32 -0500

    I don 't know what I did but it's a change so it must be good.

--- testfiles/c++/test2.cpp
+++ testfiles/c++/test2.cpp
@@ -2,16 +2,16 @@
 #include <math.h>
 
 void find_missing(int array[], int n){
-  //takes O(2^n)time and takes the  memory of two of the arrays
-  int size = pow(2,n);
-  bool found[size];
-  for (int x=0;x<size;x++)found[x]=0;
-  for (int x=0;x<size-2;x++)found[array[x]]=1;
-  for (int x=0;x<size;x++) if(!found[x])std::cout<<x<<std::endl;
+	//takes O(2^n)time and takes the  memory of two of the arrays
+	int size = pow(2,n);
+	bool found[size];
+	for (int x=0;x<size;x++)found[x]=0;
+	for (int x=0;x<size-2;x++)found[array[x]]=1;
+	for (int x=0;x<size;x++) if(!found[x])std::cout<<x<<std::endl;
 }
 
 int main(){
-  int v[]={0,1,3,4,5,6};
-  find_missing( v,3);
-  return 0;
+	int v[]={0,1,3,4,5,6};
+	find_missing( v,3);
+	return 0;
 }
